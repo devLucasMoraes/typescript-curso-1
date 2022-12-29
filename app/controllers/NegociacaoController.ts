031b0e6dc64ce590d586bc6fbec007ca048a6ade
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-29 17:14:48 -0300

    adicionado mensageView

--- app/controllers/NegociacaoController.ts
+++ app/controllers/NegociacaoController.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from "../models/Negociacao.js";
 import { Negociacoes } from "../models/Negociacoes.js";
+import { MensagemView } from "../views/MensagemView.js";
 import { NegociacoesView } from "../views/NegociacoesView.js";
 
 export class NegociacaoController {
@@ -8,6 +9,7 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
     private negociacoes: Negociacoes = new Negociacoes();
     private negociacoesView: NegociacoesView = new NegociacoesView('[data-negociacoesView]');
+    private mensagemView: MensagemView = new MensagemView('[data-mensagem-view]');
 
     constructor() {
         this.inputData = document.querySelector('[data-data]')
@@ -25,7 +27,7 @@ export class NegociacaoController {
         //this.negociacoes.lista().pop()
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
-
+        this.mensagemView.update('Negociação adicionada com sucesso');
         console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
