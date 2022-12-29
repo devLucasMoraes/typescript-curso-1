7a820fe2afffc92ed984f71b7287e8017e460f8e
devLucasMoraes <104109951+devLucasMoraes@users.noreply.github.com>
2022-12-29 14:19:32 -0300

    Delete dist/js directory

--- dist/js/app.js
+++
@@ -1,7 +0,0 @@
-import { NegociacaoController } from "./controllers/NegociacaoController.js";
-const controller = new NegociacaoController();
-const form = document.querySelector('[data-form]');
-form.addEventListener('submit', evento => {
-    evento.preventDefault();
-    controller.adiciona();
-});
