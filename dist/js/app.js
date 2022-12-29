8a8e629623dc4597933288a4ff709d876b705e31
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-28 23:52:11 -0300

    alterando o .gitignore

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,7 @@
-import { Negociacao } from "./models/Negociacao.js";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.data);
+import { NegociacaoController } from "./controllers/NegociacaoController.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('[data-form]');
+form.addEventListener('submit', evento => {
+    evento.preventDefault();
+    controller.adiciona();
+});
